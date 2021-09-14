af0b3e95309acd512c28e3863c9afc506ed9db2b
cavilosa <cavilosa33@gmail.com>
2021-09-14 08:40:37 -0600

    js

---
+++ testingJs/public/js/app.js
@@ -0,0 +1,34 @@
+let auth0 = null;
+
+const fetchAuthConfig = () => fetch("/auth_config.json");
+
+const configureClient = async () => {
+    const response = await fetchAuthConfig();
+    const config = await response.json();
+  
+    auth0 = await createAuth0Client({
+      domain: config.domain,
+      client_id: config.clientId
+    });
+  };
+
+  window.onload = async () => {
+      await configureClient();
+
+      updateUI();
+  };
+
+
+const updateUI = async () => {
+    const isAuthenticated = await auth0.isAuthenticated();
+  
+    document.getElementById("btn-logout").disabled = !isAuthenticated;
+    document.getElementById("btn-login").disabled = isAuthenticated;
+  };
+
+
+  const login = async () => {
+    await auth0.loginWithRedirect({
+      redirect_uri: window.location.origin
+    });
+  };
