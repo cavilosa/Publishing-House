49eb2c61a712860c07d17af67957f7bf2ff73a90
cavilosa <cavilosa33@gmail.com>
2021-10-04 12:16:29 -0600

    added heroku db

--- models.py
+++ models.py
@@ -12,11 +12,12 @@ import json
 load_dotenv()
 
 password = os.environ["PASSWORD"]
-# password = os.environ.get("PASSWORD")
 database_name = "publishing_house"
-database_path = "postgresql://{}:{}@{}/{}".format('cavilosa', password,
-                                                  'localhost:5432',
-                                                  database_name)
+# database_path = "postgresql://{}:{}@{}/{}".format('cavilosa', password,
+#                                                   'localhost:5432',
+#                                                   database_name)
+
+database_path = os.environ["DATABASE_URL"]
 
 Base = declarative_base()
 
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-# AUTH0 
+# AUTH0
 
 export AUTH0_DOMAIN="korzhyk-app.us.auth0.com"
 export JWT_CODE_SIGNING_SECRET="UwbJRQgpYJI9Sq_6i2GjuR10a8cSG9luenl6geSoOGgFXho7PjvRw5A39OC6e56q"
@@ -10,9 +10,11 @@ export AUTH0_CALLBACK_URL="http://localhost:5000"
 export AUTH0_LOGIN_URL="https://127.0.0.1:5000"
 export AUTH0_LOGOUT_URL="https://localhost:5000"
 
-# AUTH0 tokens 
+# AUTH0 tokens
 
  JWT_TOKEN=""
 
-# DATABASE 
+# DATABASE
 export PASSWORD="cavilosa1"
+
+export DATABASE_URL='postgres://xzlulntdewogtm:f2b1eac6e03064d0d75a27e8c872ebeeb5863c8917d306d8c7250f21c1e3843c@ec2-54-204-148-110.compute-1.amazonaws.com:5432/d3ps3306cfe4ai'
