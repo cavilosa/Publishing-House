b1203a86b350b838e851b50db1403b4cd3805427
cavilosa <cavilosa33@gmail.com>
2021-10-04 14:48:08 -0600

    changed all localhosts to herolu https

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ export AUTH0_ID="a0mzLPX0PZ6KPWVGo058FFCUUNwShqIN"
 export ALGORITHMS=["RS256"]
 export API_AUDIENCE="app"
 export CLIENT_ID="a0mzLPX0PZ6KPWVGo058FFCUUNwShqIN"
-export AUTH0_CALLBACK_URL="http://localhost:5000"
-export AUTH0_LOGIN_URL="https://127.0.0.1:5000"
-export AUTH0_LOGOUT_URL="https://localhost:5000"
+export AUTH0_CALLBACK_URL="https://fsnd-capstone-publishing-house.herokuapp.com/"
+export AUTH0_LOGIN_URL="https://fsnd-capstone-publishing-house.herokuapp.com/"
+export AUTH0_LOGOUT_URL="https://fsnd-capstone-publishing-house.herokuapp.com/"
 
 # AUTH0 tokens
 
