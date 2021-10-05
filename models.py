9bdaf663a6c9e8a707bfa5f42669f176b2bfc783
cavilosa <cavilosa33@gmail.com>
2021-10-05 09:28:09 -0600

    if on_heroku == True:

--- models.py
+++ models.py
@@ -14,12 +14,11 @@ load_dotenv()
 password = os.environ["PASSWORD"]
 database_name = "publishing_house"
 on_heroku = os.environ["on_heroku"]
-database_path = os.environ["DATABASE_URL"]
-# database_path = "postgresql://{}:{}@{}/{}".format('cavilosa', password,
-#                                                   'localhost:5432',
-#                                                   database_name)
-# if on_heroku == True:
-#     database_path = DATABASE_URL
+if on_heroku == True:
+    database_path = os.environ["DATABASE_URL"]
+database_path = "postgresql://{}:{}@{}/{}".format('cavilosa', password,
+                                                  'localhost:5432',
+                                                  database_name)
 
 print("ON HEROKU", on_heroku)
 print("database path", database_path)
