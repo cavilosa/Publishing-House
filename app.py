62fcb115aaa0e62b46feb5c515fc83f412774142
cavilosa <cavilosa33@gmail.com>
2021-10-05 14:47:48 -0600

    coreected typo

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def create_app(test_config=None):
     @app.route('/login')
     def login():
         return auth0.authorize_redirect(
-            redirect_uri=" https://fsnd-capstone-publishing-house.herokuapp.com//callback", audience="app")
+            redirect_uri=" https://fsnd-capstone-publishing-house.herokuapp.com/callback", audience="app")
 
     @app.route('/callback')
     def callback_handling():
